e6c31afed21daa554a0ba3ddc27dd3bc373f8787
Nikita Pivovarov <nikita.pivovarov2003@gmail.com>
2023-08-28 18:03:08 +0300

    add: constants, states enums

---
+++ PracticalTestTask/PracticalTestTask/Support/States.swift
@@ -0,0 +1,17 @@
+//
+//  States.swift
+//  PracticalTestTask
+//
+//  Created by Никита Пивоваров on 28.08.2023.
+//
+
+import Foundation
+
+enum States {
+    enum LoadingStates {
+        case idle
+        case loading
+        case failed
+        case loaded
+    }
+}
